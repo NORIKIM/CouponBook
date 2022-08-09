4a34a4921dd8b93fd1c5b4e5a80ffd4c56cf0a79
NORIKIM <jinagold.k@gmail.com>
2022-08-09 11:54:01 +0900

    쿠폰 추가 화면 ui 변경

--- CouponBook/CouponBook/VC/AddVC.swift
+++ CouponBook/CouponBook/VC/AddVC.swift
@@ -11,19 +11,47 @@ import CoreData
 class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDelegate {
     var manageObjectContext: NSManagedObjectContext!
     var coupon: Coupon!
+    var category: UIButton!
+    
     // Outlet
-    @IBOutlet weak var categoryTF: UITextField!
     @IBOutlet weak var nameTF: UITextField!
-    @IBOutlet weak var expiryDateLB: UILabel!
+    @IBOutlet weak var expiryDateTF: UITextField!
     @IBOutlet weak var priceTF: UITextField!
+    @IBOutlet weak var addPhotoBTN: UIButton!
+    @IBOutlet weak var img1View: UIView!
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var img2View: UIView!
+    @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var img3View: UIView!
+    @IBOutlet weak var img3: UIImageView!
+    @IBOutlet weak var img4View: UIView!
+    @IBOutlet weak var img4: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let expiryDateGesture = UITapGestureRecognizer(target: self, action: #selector(showDatePicker))
-        expiryDateLB.addGestureRecognizer(expiryDateGesture)
+        expiryDateTF.addGestureRecognizer(expiryDateGesture)
         
+        addPhotoBTN.layer.cornerRadius = 20
+        addPhotoBTN.layer.borderWidth = 1
+        addPhotoBTN.layer.borderColor = UIColor(red: 124, green: 125, blue: 125, alpha: 1).cgColor
+        if #available(iOS 15.0, *) {
+            addPhotoBTN.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 13, trailing: 10)
+        } else {
+            addPhotoBTN.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 13, right: 10)
+        }
         
+        for img in [img1View,img2View,img3View,img4View] {
+            let borderLayer = CAShapeLayer()
+            borderLayer.strokeColor = UIColor.black.cgColor
+            borderLayer.lineDashPattern = [4, 4]
+            borderLayer.frame = img!.bounds
+            borderLayer.fillColor = nil
+            borderLayer.path = UIBezierPath(roundedRect: img!.bounds, cornerRadius: 4).cgPath
+            img!.layer.addSublayer(borderLayer)
+        }
     }
 
     @objc func showDatePicker() {
@@ -38,15 +66,24 @@ class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDel
     }
     
     // MARK: - IBAction
+    @IBAction func categoryTap(_ sender: UIButton) {
+        if sender != category {
+            sender.backgroundColor = UIColor(red: 210, green: 249, blue: 245, alpha: 1)
+            if category != nil {
+                category.backgroundColor = UIColor.white
+            }
+            category = sender
+        }
+    }
+    
     @IBAction func registTap(_ sender: UIButton) {
         if self.coupon == nil {
             self.coupon = (NSEntityDescription.insertNewObject(forEntityName: Coupon.entityName,
-                                                                 into: self.manageObjectContext) as! Coupon)
+                                                               into: self.manageObjectContext) as! Coupon)
         }
-        
-        self.coupon.category = categoryTF.text!
+        self.coupon.category = (category.titleLabel?.text)!
         self.coupon.name = nameTF.text!
-        self.coupon.expiryDate = expiryDateLB.text!
+        self.coupon.expiryDate = expiryDateTF.text!
         self.coupon.price = priceTF.text
         
         do {
@@ -66,10 +103,10 @@ class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDel
             self.present(alert, animated: true, completion: nil)
         }
     }
-    
+        
     // MARK: - DatePickerDelegate
     func selectDate(str: String) {
-        expiryDateLB.text = str
+        expiryDateTF.text = str
     }
     
 }
