aed7929036ef13692d843eb549806029dd9da08b
NORIKIM <jinagold.k@gmail.com>
2022-08-16 13:37:54 +0900

    사진 추가 및 삭제 로직 수정

--- CouponBook/CouponBook/VC/AddVC.swift
+++ CouponBook/CouponBook/VC/AddVC.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 import CoreData
+import PhotosUI
+import BSImagePicker
 
 class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDelegate, UITextFieldDelegate {
+    // property
     var manageObjectContext: NSManagedObjectContext!
     var coupon: Coupon!
     var category: UIButton!
-    
+//    var photoSuperArr = [UIView]()
+    var photoArr = [UIImageView]()
+    var clearArr = [UIButton]()
+    var photos = [UIImage]()
+
     // Outlet
     @IBOutlet weak var scroll: UIScrollView!
     @IBOutlet weak var nameTF: UITextField!
@@ -22,12 +29,16 @@ class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDel
     @IBOutlet weak var addPhotoBTN: UIButton!
     @IBOutlet weak var img1View: UIView!
     @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var clear1BTN: UIButton!
     @IBOutlet weak var img2View: UIView!
     @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var clear2BTN: UIButton!
     @IBOutlet weak var img3View: UIView!
     @IBOutlet weak var img3: UIImageView!
+    @IBOutlet weak var clear3BTN: UIButton!
     @IBOutlet weak var img4View: UIView!
     @IBOutlet weak var img4: UIImageView!
+    @IBOutlet weak var clear4BTN: UIButton!
     /// 메모
     @IBOutlet weak var memoTV: UITextView!
     
@@ -74,16 +85,22 @@ class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDel
         } else {
             addPhotoBTN.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 13, right: 10)
         }
+//        photoSuperArr = [img1View, img2View, img3View, img4View]
+        photoArr = [img1,img2,img3,img4]
+        clearArr = [clear1BTN, clear2BTN, clear3BTN, clear4BTN]
+        if #available(iOS 14.0, *) {
+            
+        }
         
         // 이미지뷰 점선 테두리 적용
-        for img in [img1View,img2View,img3View,img4View] {
+        for view in [img1,img2,img3,img4] {
             let borderLayer = CAShapeLayer()
             borderLayer.strokeColor = UIColor.black.cgColor
             borderLayer.lineDashPattern = [4, 4]
-            borderLayer.frame = img!.bounds
+            borderLayer.frame = view!.bounds
             borderLayer.fillColor = nil
-            borderLayer.path = UIBezierPath(roundedRect: img!.bounds, cornerRadius: 4).cgPath
-            img!.layer.addSublayer(borderLayer)
+            borderLayer.path = UIBezierPath(roundedRect: view!.bounds, cornerRadius: 4).cgPath
+            view!.layer.addSublayer(borderLayer)
         }
         
         // 메모 입력창
@@ -176,9 +193,75 @@ class AddVC: UIViewController, NSFetchedResultsControllerDelegate, DatePickerDel
         return true
     }
     
-    // MARK: - IBAction - 사진 추가
+    // MARK: - 사진 추가
     @IBAction func photoPicTap(_ sender: UIButton) {
+        let imagePicker = ImagePickerController()
+        imagePicker.settings.selection.max = 4 - photos.count
+        imagePicker.settings.theme.selectionStyle = .numbered
+        imagePicker.settings.fetch.assets.supportedMediaTypes = [.image]
+        imagePicker.settings.selection.unselectOnReachingMax = false
         
+        let start = Date()
+        
+        self.presentImagePicker(imagePicker, select: { (asset) in
+            print("Selected: \(asset)")
+            let img = self.getAssetThumbnail(asset: asset)
+            self.photos.append(img)
+        }, deselect: { (asset) in
+            print("Deselected: \(asset)")
+            let img = self.getAssetThumbnail(asset: asset)
+            guard let idx = self.photos.firstIndex(of: img) else { return }
+            self.photos.remove(at: idx)
+        }, cancel: { (assets) in
+            print("Canceled with selections: \(assets)")
+        }, finish: { (assets) in
+            print("Finished with selections: \(assets)")
+            for idx in 0 ..< self.photos.count {
+                self.photoArr[idx].image = self.photos[idx]
+                self.clearArr[idx].isHidden = false
+            }
+        }, completion: {
+            let finish = Date()
+            print(finish.timeIntervalSince(start))
+        })
+    }
+    
+    // PHAsset -> UIImage
+    func getAssetThumbnail(asset: PHAsset) -> UIImage {
+        let manager = PHImageManager.default()
+        let option = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        option.isSynchronous = true
+        manager.requestImage(for: asset, targetSize: CGSize(width: 40, height: 40), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+            thumbnail = result!
+        })
+        return thumbnail
+    }
+    
+    // 사진 삭제
+    @IBAction func clearPhotoTap(_ sender: UIButton) {
+        let tag = sender.tag
+        
+        // 이미지가 하나
+        if photos.count == 1 || tag == photos.count - 1 {
+            photoArr[tag].image = nil
+            clearArr[tag].isHidden = true
+        // 이미지가 2개 이상
+        } else  {
+            for img in tag ..< photos.count {
+                if img != photos.count - 1 {
+                    photoArr[img].image = photoArr[img+1].image
+                    photoArr[img+1].image = nil
+                    clearArr[img+1].isHidden = true
+                }
+                if photoArr[img].image != nil {
+                    clearArr[img].isHidden = false
+                } else {
+                    clearArr[img].isHidden = true
+                }
+            }
+        }
+        photos.remove(at: tag)
     }
     
     // MARK: - IBAction - 등록
