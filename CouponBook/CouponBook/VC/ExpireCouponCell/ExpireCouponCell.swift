7772e18f1b49b49d76e90a370ad6f26b1cac6b05
NORIKIM <jinagold.k@gmail.com>
2022-08-26 12:40:51 +0900

    임박쿠폰 셀

---
+++ CouponBook/CouponBook/VC/ExpireCouponCell/ExpireCouponCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExpireCouponCell.swift
+//  CouponBook
+//
+//  Created by haniln on 2022/08/24.
+//
+
+import UIKit
+
+class ExpireCouponCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
